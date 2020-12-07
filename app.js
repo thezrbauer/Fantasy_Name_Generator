880ddaf5d8cb3affefc2cce4000608e7e1594b68
Zack Bauer <zack.bauer30@gmail.com>
2020-12-07 10:53:10 -0500

    Established the goals of the project and began the html

---
+++ app.js
@@ -0,0 +1,5 @@
+/* 1. Create a Name generator */
+/* 2. Find an api and connect to that api */
+/* 3. Make sure it works */
+/* 4. Use jquery to spice up the page */
+/* 5. Create a functional Random name generator app */
